dd3516b226d7fa76a742827ad493d192349174a4
JrColas <jrcolas@outlook.com>
2019-02-22 19:14:36 -0500

    Completed Ex11-12

---
+++ LPTHW/ex11.py
@@ -0,0 +1,12 @@
+# Exercise 11: Asking questions
+
+print "How old are you?",
+age = raw_input()
+print "How tall are you?",
+height = raw_input()
+print "How much do you weigh?",
+weight = raw_input()
+
+print "So, you're %r years old, %r tall and weigh %r lbs." % (
+    age, height, weight
+)
---
+++ LPTHW/ex12.py
@@ -0,0 +1,9 @@
+# Exercise 12: Prompting people
+
+age = raw_input("How old are you? ")
+height = raw_input("How tall are you? ")
+weight = raw_input("How much do you weigh? ")
+
+print "So, you're %r years old, %r tall and weigh %r lbs." % (
+    age, height, weight
+)
