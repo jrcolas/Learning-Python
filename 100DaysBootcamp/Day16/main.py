c23d776bdd795f88ac50edcccd4d0161d4eaa378
Franck Colas <jrcolas@gmail.com>
2022-11-25 17:30:26 -0500

    Started day 16

---
+++ 100DaysBootcamp/Day16/main.py
@@ -0,0 +1,11 @@
+# Start of day 16
+import turtle
+
+timmy = turtle.Turtle()
+timmy.shape("turtle")
+timmy.color("cyan3")
+timmy.forward(100)
+
+my_screen = turtle.Screen()
+print(my_screen.canvheight)
+my_screen.exitonclick()
