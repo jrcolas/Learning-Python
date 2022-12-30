0a8bc205226d1c49afb4717ef4fe652edd77285c
JrColas <jrcolas@gmail.com>
2022-12-30 08:54:27 -0500

    Day 21 in progress

--- 100DaysBootcamp/Day20/main.py
+++ 100DaysBootcamp/Day20/main.py
@@ -1,6 +1,7 @@
 from snake import Snake
 import time
-from turtle import Turtle, Screen
+from turtle import Screen
+from food import Food
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -8,6 +9,7 @@ screen.bgcolor("black")
 screen.title("Best Snake Game")
 
 snake = Snake()
+food = Food()
 
 screen.listen()
 screen.onkey(snake.up, "Up")
@@ -20,15 +22,15 @@ game_is_on = True
 while game_is_on:
     screen.update()
     time.sleep(0.1)
-
     snake.move()
 
+    # TODO Detect collision with food
+    if snake.head.distance(food) < 15:
+        food.refresh()
 
 
+    # TODO Create a scoreboard
 
-
-# TODO Detect collision with food
-# TODO Create a scoreboard
 # TODO Detect collision with wall
 # TODO Detect collision with tail
 
