b73755797fdb307a2b20d4a561a902dd1b08a3cf
JrColas <jrcolas@gmail.com>
2023-01-07 10:51:16 -0500

    Day 26 Nato phonetic spell helper thingy

--- 100DaysBootcamp/Day26/main.py
+++ 100DaysBootcamp/Day26/main.py
@@ -25,3 +25,32 @@ names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
 cap_names = [name for name in names if len(name) > 5]
 cap_names = [name.upper() for name in names if len(name) > 5]
 
+# # Dictionary Comprehension
+new_dict = {new_key:new_value for (key, value) in dict.items()}
+
+names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+import random
+student_score = {student:random.randint(1, 100) for student in names}
+passed_students = {student:score for (student, score) in student_score.items() if score >= 70}
+
+
+# # Looping through dictionaries:
+# for (key, value) in student_dict.items():
+#     print(value)
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+# Looping through a data frame
+# for (key, value) in student_data_frame.items():
+#     print(value)
+# Loop through rows of a data frame
+for(index, row) in student_data_frame.iterrows():
+    print(index)
+    print(row)
+    print(row.student)
+    print(row.score)
+    if row.student == "Angela":
+        print(row.score)
