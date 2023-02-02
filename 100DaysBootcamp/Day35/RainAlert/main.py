d2c98565f99cef6fd40518691f6cdb329854b645
JrColas <jrcolas@gmail.com>
2023-02-02 16:57:23 -0500

    Day35 - Removed API key from file

--- 100DaysBootcamp/Day35/RainAlert/main.py
+++ 100DaysBootcamp/Day35/RainAlert/main.py
@@ -1,6 +1,6 @@
 import requests
 
-api_key = "f5912a48d4773b4c4065704428c281af"
+api_key = "someapikey"
 OWM_Endpoint = "https://api.openweathermap.org/data/3.0/onecall"
 
 weather_params = {
@@ -9,7 +9,7 @@ weather_params = {
     "appid": api_key,
 }
 
-url = "https://api.openweathermap.org/data/3.0/onecall?lat=25.761681&lon=-80.191788&appid=806c516ce240b6fb47652ca601cbb1d4"
+url = "https://api.openweathermap.org/data/3.0/onecall?lat=25.761681&lon=-80.191788&appid=someapikey"
 
 response = requests.get(OWM_Endpoint, params=weather_params)
 response.raise_for_status()
